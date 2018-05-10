15ec75e06e8e452c8a9456c5b719f8792949f412
jyamashiro <jyamashiro@linkedin.com>
2018-05-10 10:50:50 -0700

    #5 initial implementation of pug

--- server/index.js
+++ server/index.js
@@ -12,10 +12,21 @@ const http = require('http').createServer(app);
 app.use(staticMiddleware);
 app.use(bodyParser.json());
 
+// Temporary classes for testing
+// Nothing in production should be here
+const pug = require('pug');
+const templatePath = require('path').join(__dirname, '/email_templates');
+
 // Testing endpoint to return time as string
 app.get('/test', (req, res) => {
   const timeNow = new Date();
   res.send('the datetime is: ' + timeNow);
+})
+
+app.get('/pug', (req, res) => {
+  res.send(pug.renderFile(templatePath + '/template.pug', {
+    first_name: 'FIRST NAME'
+  }))
 })
 
 http.listen(PORT || 5760, () => {
