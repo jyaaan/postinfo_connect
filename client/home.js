9b7db7ef51ac273006d795c13d2c527ff3089519
jyamashiro <jyamashiro@linkedin.com>
2018-05-10 15:30:36 -0700

    adding babelrc and more ui elements #5

--- client/home.js
+++ client/home.js
@@ -5,6 +5,7 @@ const async = require('async');
 const Home = props => {
   return (
     <div className='ui eight column centered row'>
+      <p>hello!</p>
       <img className='ui large image centered column' src={'/images/logo_main.png'} />
     </div>
   )
